602dd8c7b4c042722cf2ded29bde5c577eeb5163
voyager003 <miraclex000@gmail.com>
2025-08-21 11:21:00 +0900

    refactor: spare 리팩토링

--- src/main/kotlin/bowling/Game.kt
+++ src/main/kotlin/bowling/Game.kt
@@ -11,10 +11,15 @@ class Game {
     fun score(): Int {
         var score = 0
         var i = 0
-        for(frame in 0 until 10)
-            score += rolls[i] + rolls[i+1]
-            i += 2
 
+        for(frame in 0 until 10)
+            if (rolls[i] + rolls[i+1] == 10) {
+                score += 10 + rolls[i+2]
+                i += 2
+            } else {
+                score += rolls[i] + rolls[i+1]
+                i += 2
+            }
         return score
     }
 }
