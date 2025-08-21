02bfe9c4a6b99ab9eadf1817fe99339e17e4f526
voyager003 <miraclex000@gmail.com>
2025-08-21 10:23:32 +0900

    build: 테스트 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,11 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    // Junit
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+    // AssertJ
+    testImplementation("org.assertj:assertj-core:3.26.3")
 }
 
 tasks.test {
